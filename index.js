4309a6f42681dfe2bc2b7cb138113437b0875eae
msetu1 <msetu5763@gmail.com>
2023-11-08 02:43:17 +0600

    delete assignment

--- index.js
+++ index.js
@@ -62,12 +62,12 @@ async function run() {
         })
 
         // deleted
-        // app.delete('/allasignment/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await allAssignmentCollection.deleteOne(query)
-        //     res.send(result)
-        // })
+        app.delete('/allasignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await allAssignmentCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
         // update 
